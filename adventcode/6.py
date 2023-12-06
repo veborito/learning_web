b30746f3d2cc7c1d72ed0d1b6b15b180a9d0542b
Boris Verdecia Echarte <borisverdeciaecharte@gmail.com>
2023-12-06 10:47:43 +0100

    day 6

---
+++ adventcode/6.py
@@ -0,0 +1,13 @@
+file = open("inputs/input_6")
+lines = [line.strip() for line in file]
+time = int("".join([n for n in lines[0].split(' ') if n.isnumeric()]))
+distance = int("".join([n for n in lines[1].split(' ') if n.isnumeric()]))
+
+r = 0
+for i in range(time + 1):
+    front = i * (time - i)
+    if front > distance:
+        r = i
+        break
+
+print(time + 1 - 2 * r)
